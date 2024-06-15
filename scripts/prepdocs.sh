aec4b1846706626e554a4525412d4cfa9319c297
Vaibbavi Senthil Kumar <vaibbavis@microsoft.com>
2024-06-14 20:19:16 -0700

    fix path issue

--- scripts/prepdocs.sh
+++ scripts/prepdocs.sh
@@ -66,7 +66,7 @@ fi
 echo "TESTING WHY WONT U WORK"
 
 if [ "$BRING_YOUR_OWN_DATA" = true ]; then
-  files="./$CUSTOM_DATA_PATH/*"
+  files="./../$CUSTOM_DATA_PATH/*"
   echo "BRING_YOUR_OWN_DATA is set to true."
 else
   files="./data/*"
